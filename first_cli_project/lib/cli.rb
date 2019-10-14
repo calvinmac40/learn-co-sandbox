39ac062aa9106ca1c65b36b35a3820853d67e1b8
Calvin McDowell <calvinmac40@hotmail.com>
2019-10-14 02:08:13 +0000

    build scraper class

--- first_cli_project/lib/cli.rb
+++
@@ -1,3 +0,0 @@
-class Cli 
-  
-end
--- first_cli_project/lib/scraper.rb
+++ first_cli_project/lib/scraper.rb
@@ -2,3 +2,5 @@ require "pry-nav"
 require "nokogiri"
 require "open-uri"
 
+@doc = Nokogiri::HTML(open("https://www.craigslist.org/about/sites#US/"))
+binding.pry
