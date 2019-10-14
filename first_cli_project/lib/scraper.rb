499c50926f355ea68b362278cc75a5b36c755574
Calvin McDowell <calvinmac40@hotmail.com>
2019-10-14 02:44:50 +0000

    scraper build

--- first_cli_project/lib/scraper.rb
+++ first_cli_project/lib/scraper.rb
@@ -2,5 +2,7 @@ require "pry-nav"
 require "nokogiri"
 require "open-uri"
 
-@doc = Nokogiri::HTML(open("https://www.craigslist.org/about/sites#US/"))
+@doc = Nokogiri::HTML(open("https://smd.craigslist.org/d/real-estate/search/rea"))
+# listing_title = @doc.css(".result-title.hdrlnk")
+#listing_price = @doc.css("span.result-price").first.text
 binding.pry
